b41b090dfc3d4ef0270fe0cd9efe1bfa71dd1d34
NDCT94 <nathan.dumas01@gmail.com>
2020-10-16 12:00:11 -0400

    almost done with form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
-import React from "react"
-import Homepage from './components/Homepage'
-import Form from './components/Form'
+import React, { useState, useEffect } from "react";
+import Homepage from "./components/Homepage";
+import Form from "./components/Form";
+import axios from 'axios'
+
+
+const initialValues = {
+  name: "",
+  size: "",
+  pepperoni: false,
+  sausage: false,
+  mushrooms: false,
+  peppers: false,
+  instructions: "",
+};
+
+const initialErrors = {
+  name: "",
+};
+
+const initialData = [];
+const initialDisabled = true;
+
 const App = () => {
+
+  const [data, setData] = useState(initialData)
+  const [formValues, setFormValues] = useState(initialValues)
+  const [formErrors, setFormErrors] = useState(initialErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const orderPizza = newOrder => {
+    axios
+    .post('https://reqres.in/api/pizza')
+    .then(res => setData(...data, res.data))
+    .catch(err => console.log(err))
+  }
+
+  const inputChange = (name, value) => {
+  }
+
+  const submitForm = () => {
+    const newOrder = {
+      name: formValues.name.trim(),
+      toppings: ['pepperoni', 'sausage', 'mushrooms', 'pineapple'].filter(toppings => formValues[toppings])
+    }
+    orderPizza(newOrder)
+    setFormValues(initialValues)
+  }
+
   return (
     <>
       <Homepage />
-      <Form />
+      <Form 
+        values={formValues}
+        submit={submitForm}
+        change={inputChange}
+        disabled={disabled}
+        errors={formErrors}
+      />
     </>
-  )
-}
-export default App
+  );
+};
+export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,78 @@
 import React from "react";
 
 export default function Form(props) {
+  const { values, submit, change, disabled, errors } = props;
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+
+  const onChange = (evt) => {
+    const { name, value, type, checked } = evt.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse);
+  };
   return (
-    <form>
-      <label htmlFor='name'>Name:
-        <input name="name" type="text" placeholder="Name" />
+    <form onSubmit={onSubmit}>
+      <label htmlFor="name">
+        Name:
+        <input
+          name="name"
+          value={values.name}
+          onChange={onChange}
+          type="text"
+          placeholder="Name"
+        />
       </label>
-      <label htmlFor='size'>
-          <select name='size'>
-              <option>Small</option>
-              <option>Medium</option>
-              <option>Large</option>
-              <option>Extra Large</option>
-          </select>
+      <label htmlFor="size">
+        Size:
+        <select name="size">
+          <option>Small</option>
+          <option>Medium</option>
+          <option>Large</option>
+          <option>Extra Large</option>
+        </select>
+      </label>
+      <label>
+        Toppings: Pepperoni
+        <input
+          name="pepperoni"
+          type="checkbox"
+          checked={values.pepperoni}
+          onchange={onChange}
+        />
+        Sausage
+        <input
+          name="sausage"
+          type="checkbox"
+          value={values.sausage}
+          onchange={onChange}
+        />
+        Mushrooms
+        <input
+          name="mushrooms"
+          type="checkbox"
+          value={values.mushrooms}
+          onchange={onChange}
+        />
+        Pineapple
+        <input
+          name="pineapple"
+          type="checkbox"
+          value={values.pineapple}
+          onchange={onChange}
+        />
+      </label>
+      <label>
+        Special Instructions:
+        <input
+          type="text"
+          name="instructions"
+          placeholder="Instructions"
+          value={values.instructions}
+          onchange={onChange}
+        />
       </label>
       <button>Add to order</button>
     </form>
